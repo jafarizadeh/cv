12e0025ac8dab75149d8b4363709105f4bbb1045
Mehdi <mehdi0341@gmail.com>
2023-06-18 12:23:51 +0200

    submit general info

--- controllers/connection.php
+++ controllers/connection.php
@@ -1,3 +1,10 @@
 <?php
 $con = mysqli_connect('localhost', 'root', '', 'cv') or die('Connection Lost');
+
+function getGeneralInfo($conection)
+{
+    $select = mysqli_query($conection, "SELECT * FROM `general_info` LIMIT 1");
+    $data = mysqli_fetch_array($select);
+    return $data;
+}
 ?>
