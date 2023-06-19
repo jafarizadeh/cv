16292b493a269fd4c389870738e95ba0645571b4
Mehdi <mehdi0341@gmail.com>
2023-06-19 15:24:39 +0200

    modules array

--- panel/controllers/router.php
+++ panel/controllers/router.php
@@ -3,6 +3,13 @@ class Router
 {
     private $module_folder_name = 'modules';
     private $main_module_name = 'index.php?module=main';
+    private $modules =
+    [
+        'main' => 'main.php',
+        'setting' => 'setting.php',
+        'default' => '404.php'
+    ];
+
     function Router()
     {
 
@@ -19,17 +26,14 @@ class Router
         }
         else
         {
-            switch($module_value)
+            $myModules = $this -> modules;
+            if (!isset($myModules[$module_value]))
             {
-                case 'main':
-                    include_once "$folder/main.php";
-                    break;
-                case 'setting':
-                    break;
-                default:
-                    include_once "$folder/404.php";
-                    // load 404 page
-
+                include_once $folder.'/'.$myModules['default'];
+            }
+            else
+            {
+                include_once $folder.'/'.$myModules[$module_value];
             }
         }
     }
