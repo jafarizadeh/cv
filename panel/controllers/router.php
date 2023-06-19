d73180aa956caf767d8f909342060b2a9d63742e
Mehdi <mehdi0341@gmail.com>
2023-06-19 13:06:43 +0200

    module folder

--- panel/controllers/router.php
+++ panel/controllers/router.php
@@ -1,6 +1,7 @@
 <?php
 class Router
 {
+    private $module_folder_name = 'modules';
     function Router()
     {
 
@@ -9,15 +10,16 @@ class Router
     function route()
     {
         $module_value = getParam('module');
+        $folder = $this->module_folder_name;
         switch($module_value)
         {
             case 'main':
-                include_once "modules/main.php";
+                include_once "$folder/main.php";
                 break;
             case 'setting':
                 break;
             default:
-                include_once "modules/404.php";
+                include_once "$folder/404.php";
                 // load 404 page
 
         }
