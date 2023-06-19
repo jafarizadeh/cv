cbb3812209c58a3ef37a331b0c7c2219166d2ef5
Mehdi <mehdi0341@gmail.com>
2023-06-20 00:21:55 +0200

    finish experience add new

--- panel/modules/add_exp.php
+++ panel/modules/add_exp.php
@@ -2,34 +2,26 @@
 $select = mysqli_query($GLOBALS['con'], "SELECT * FROM `experience` WHERE `id` = '1' limit 1");
 $general_info_data = mysqli_fetch_array($select);
 
-function updatePublicSettingTable()
+function addExpTable()
 {
-    if (postParam('submitPublicSettingForm') != null) {
-        $web_title = postParam('webTitle') != null ? postParam('webTitle') : '';
-        $first_name = postParam('firstName') != null ? postParam('firstName') : '';
-        $last_name = postParam('lastName') != null ? postParam('lastName') : '';
-        $telephone_number = postParam('tel') != null ? postParam('tel') : '';
+    if (postParam('submitAddExpForm') != null) {
+        $title = postParam('title') != null ? postParam('title') : '';
+        $sub_title = postParam('subTitle') != null ? postParam('subTitle') : '';
+        $content = postParam('content') != null ? postParam('content') : '';
+        $from_date = postParam('fromDate') != null ? postParam('fromDate') : '';
+        $to_date = postParam('toDate') != null ? postParam('toDate') : '';
 
-        $email_address = postParam('email') != null ? postParam('email') : '';
-        $postal_address = postParam('address') != null ? postParam('adddress') : '';
-        $facebook_id = postParam('facebook') != null ? postParam('facebook') : '';
-        $linkedin_id = postParam('linkedin') != null ? postParam('linkedin') : '';
+        if (!empty($title) || !empty($sub_title) || !empty($content)) {
+            $select = mysqli_query($GLOBALS['con'], "INSERT INTO `experience` (`title`, `subtitle`, `content`,
+            `fromDate`, `toDate`) VALUES ('$title', '$sub_title', '$content', '$from_date', '$to_date')");
 
-        $twitter_id = postParam('twitter') != null ? postParam('twitter') : '';
-        $github_id = postParam('github') != null ? postParam('github') : '';
-        $about = postParam('about') != null ? postParam('about') : '';
-        $interests = postParam('interests') != null ? postParam('interests') : '';
-
-        $select = mysqli_query($GLOBALS['con'], "UPDATE `general_info` SET `webTitle` = '$web_title',
-        `firstName` = '$first_name', `lastName` = '$last_name', `tel` = '$telephone_number', 
-        `email` = '$email_address', `address` = '$postal_address', `facebook` = '$facebook_id', 
-        `linkedin` = '$linkedin_id', `twitter` = '$twitter_id', `github` = '$github_id',
-         `about` = '$about', `interests` = '$interests'");
-
-        if ($select) {
-            return 2;
+            if ($select) {
+                return 2;
+            } else {
+                return 1;
+            }
         } else {
-            return 1;
+            return 3;
         }
     } // when clicked is statement
     else {
@@ -44,28 +36,28 @@ function updatePublicSettingTable()
         <div class="row">
             <div class="col-3">
                 <div class="form-group">
-                    <label for="fbId">title :</label>
-                    <input type="text" class="form-control form-control-user" id="fbId" name="title" placeholder="Enter Title...">
+                    <label for="titleId">title :</label>
+                    <input type="text" class="form-control form-control-user" id="titleId" name="title" placeholder="Enter Title..." value="">
                 </div>
             </div>
             <div class="col-3">
                 <div class="form-group">
-                    <label for="fbId">sub title :</label>
-                    <input type="text" class="form-control form-control-user" id="fbId" name="subTitle" placeholder="Enter Sub Title...">
+                    <label for="subTitleId">sub title :</label>
+                    <input type="text" class="form-control form-control-user" id="subTitleId" name="subTitle" placeholder="Enter Sub Title..." value="">
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-3">
                 <div class="form-group">
-                    <label for="dromDateId">from date :</label>
-                    <input type="text" class="form-control form-control-user" id="fromDateId" name="fromDate" placeholder="Date...">
+                    <label for="fromDateId">from date :</label>
+                    <input type="text" class="form-control form-control-user" id="fromDateId" name="fromDate" placeholder="Date..." value="">
                 </div>
             </div>
             <div class="col-3">
                 <div class="form-group">
-                    <label for="fbId">to date :</label>
-                    <input type="text" class="form-control form-control-user" id="toDateId" name="toDate" placeholder="Date...">
+                    <label for="toDateId">to date :</label>
+                    <input type="text" class="form-control form-control-user" id="toDateId" name="toDate" placeholder="Date..." value="">
                 </div>
             </div>
         </div>
@@ -79,16 +71,19 @@ function updatePublicSettingTable()
         </div>
         <div class="row">
             <div class="col-3">
-                <input type="submit" name="submitAddExpForm" class="btn btn-primary btn-user btn-block" value="Add Experiences">
+                <input type="submit" name="submitAddExpForm" class="btn btn-primary btn-user btn-block" value="Add Experience">
             </div>
         </div>
     </form>
     <br>
     <div id="result">
-        <?php $status = updatePublicSettingTable(); ?>
-        <?php if ($status == 2) : header('location: index.php?module=setting'); ?>
+        <?php $status = addExpTable(); ?>
+        <?php if ($status == 2) : ?>
+            <div class="alert alert-success">Experience Added Successfully.</div>
         <?php elseif ($status == 1) : ?>
-            <div class="alert alert-danger">Public Setting Updating Failed!</div>
+            <div class="alert alert-danger">Adding Experience Failed!</div>
+        <?php elseif ($status == 3) : ?>
+            <div class="alert alert-danger">Please Enter Required Filed!</div>
         <?php endif; ?>
     </div>
 </div>
