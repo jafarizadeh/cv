ddd46ecad3922178d25e9a301fd6088426914751
Mehdi <mehdi0341@gmail.com>
2023-06-19 18:39:12 +0200

    Modification Public Setting

--- panel/modules/setting.php
+++ panel/modules/setting.php
@@ -2,20 +2,19 @@
 $select = mysqli_query($GLOBALS['con'], "SELECT * FROM `general_info` WHERE `id` = '1' limit 1");
 $general_info_data = mysqli_fetch_array($select);
 
-function updatePublicSettingTable ()
+function updatePublicSettingTable()
 {
-    if (postParam('submitPublicSettingForm') != null)
-    {
+    if (postParam('submitPublicSettingForm') != null) {
         $web_title = postParam('webTitle') != null ? postParam('webTitle') : '';
         $first_name = postParam('firstName') != null ? postParam('firstName') : '';
         $last_name = postParam('lastName') != null ? postParam('lastName') : '';
         $telephone_number = postParam('tel') != null ? postParam('tel') : '';
-        
+
         $email_address = postParam('email') != null ? postParam('email') : '';
         $postal_address = postParam('address') != null ? postParam('adddress') : '';
         $facebook_id = postParam('facebook') != null ? postParam('facebook') : '';
         $linkedin_id = postParam('linkedin') != null ? postParam('linkedin') : '';
-        
+
         $twitter_id = postParam('twitter') != null ? postParam('twitter') : '';
         $github_id = postParam('github') != null ? postParam('github') : '';
         $about = postParam('about') != null ? postParam('about') : '';
@@ -26,7 +25,14 @@ function updatePublicSettingTable ()
         `email` = '$email_address', `address` = '$postal_address', `facebook` = '$facebook_id', 
         `linkedin` = '$linkedin_id', `twitter` = '$twitter_id', `github` = '$github_id',
          `about` = '$about', `interests` = '$interests'");
-        
+
+        if ($select) {
+            return 2;
+        } else {
+            return 1;
+        }
+    } else {
+        return 0;
     }
 }
 ?>
@@ -37,90 +43,74 @@ function updatePublicSettingTable ()
         <div class="row">
             <div class="col-3">
                 <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="firstnameId" aria-describedby="emailHelp" placeholder="Enter First Name...">
+                    <label for="fbId">First Name :</label>
+                    <input type="text" class="form-control form-control-user" id="fbId" name="firstName" aria-describedby="emailHelp" placeholder="Enter First Name..." value="<?= $general_info_data['firstName'] ?>">
                 </div>
             </div>
             <div class="col-3">
                 <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="lastnameId" aria-describedby="emailHelp" placeholder="Enter Last Name...">
+                    <label for="fbId">Last Name :</label>
+                    <input type="text" class="form-control form-control-user" id="fbId" name="lastName" aria-describedby="emailHelp" placeholder="Enter last Name..." value="<?= $general_info_data['lastName'] ?>">
                 </div>
             </div>
             <div class="col-3">
                 <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="phoneId" aria-describedby="emailHelp" placeholder="Enter Phone Number...">
+                    <label for="fbId">Phone Number :</label>
+                    <input type="text" class="form-control form-control-user" id="fbId" name="tel" aria-describedby="emailHelp" placeholder="Enter Phone Number..." value="<?= $general_info_data['tel'] ?>">
                 </div>
             </div>
             <div class="col-3">
                 <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="emailId" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-9">
-                <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="addressId" aria-describedby="emailHelp" placeholder="Enter Postal Address...">
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="websiteId" aria-describedby="emailHelp" placeholder="Enter Website Address...">
+                    <label for="fbId">Email Address :</label>
+                    <input type="text" class="form-control form-control-user" id="fbId" name="email" aria-describedby="emailHelp" placeholder="Enter Email Address..." value="<?= $general_info_data['email'] ?>">
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-3">
                 <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="instagramId" aria-describedby="emailHelp" placeholder="Enter Instagram Link...">
+                    <label for="fbId">Linkedin Link :</label>
+                    <input type="text" class="form-control form-control-user" id="fbId" name="Limkedin" aria-describedby="emailHelp" placeholder="Enter Linkedin Link..." value="<?= $general_info_data['firstName'] ?>">
                 </div>
             </div>
             <div class="col-3">
                 <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="linkdinId" aria-describedby="emailHelp" placeholder="Enter Linkdin Link...">
+                    <label for="fbId">Facebook Link :</label>
+                    <input type="text" class="form-control form-control-user" id="fbId" name="facebook" aria-describedby="emailHelp" placeholder="Enter Facebook Link..." value="<?= $general_info_data['facebook'] ?>">
                 </div>
             </div>
             <div class="col-3">
                 <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="facebookId" aria-describedby="emailHelp" placeholder="Enter Facebook Link...">
+                    <label for="fbId">Twitter Link :</label>
+                    <input type="text" class="form-control form-control-user" id="fbId" name="twitter" aria-describedby="emailHelp" placeholder="Enter Twitter Link..." value="<?= $general_info_data['twitter'] ?>">
                 </div>
             </div>
             <div class="col-3">
                 <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="twitterId" aria-describedby="emailHelp" placeholder="Enter Twitter Link...">
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-3">
-                <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="githubId" aria-describedby="emailHelp" placeholder="Enter Github Link...">
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="" aria-describedby="emailHelp" placeholder="">
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="" aria-describedby="emailHelp" placeholder="">
-                </div>
-            </div>
-            <div class="col-3">
-                <div class="form-group">
-                    <input type="text" class="form-control form-control-user" name="" aria-describedby="emailHelp" placeholder="">
+                    <label for="fbId">Github Link :</label>
+                    <input type="text" class="form-control form-control-user" id="fbId" name="github" aria-describedby="emailHelp" placeholder="Enter Ginhub Link..." value="<?= $general_info_data['github'] ?>">
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-6">
                 <div class="form-group">
-                    <textarea class="form-control form-control-user" name="about" placeholder="Enter about text..."></textarea>
+                    <label for="fbId">About Text :</label>
+                    <textarea class="form-control form-control-user" id="fbId" name="about" placeholder="Enter about text..." value="<?= $general_info_data['about'] ?>"></textarea>
                 </div>
             </div>
             <div class="col-6">
                 <div class="form-group">
-                    <textarea class="form-control form-control-user" name="intrest" placeholder="Enter intrest text..."></textarea>
+                    <label for="fbId">Interests Text :</label>
+                    <textarea class="form-control form-control-user" id="fbId" name="interests" placeholder="Enter interests text..." value="<?= $general_info_data['interests'] ?>"></textarea>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <label for="fbId">Address :</label>
+                    <textarea class="form-control form-control-user" id="fbId" name="address" placeholder="Enter Postal Address..." value="<?= $general_info_data['address'] ?>"></textarea>
                 </div>
             </div>
         </div>
@@ -129,5 +119,15 @@ function updatePublicSettingTable ()
                 <input type="submit" name="submitPublicSettingForm" class="btn btn-primary btn-user btn-block" value="Save Public Setting">
             </div>
         </div>
+        
     </form>
+    <br>
+    <div id="result">
+        <?php $status = updatePublicSettingTable(); ?>
+        <?php if ($status == 2) : ?>
+            <div class="alert alert-success">Public Setting Update Successfully.</div>
+        <?php elseif ($status == 1) : ?>
+            <div class="alert alert-danger">Public Setting Updating Failed!</div>
+        <?php endif ?>
+    </div>
 </div>
