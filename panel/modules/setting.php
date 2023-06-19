a5346d92c2ad95b6d65c7ca407ec7a417f38d73e
Mehdi <mehdi0341@gmail.com>
2023-06-19 16:55:19 +0200

    public setting form

--- panel/modules/setting.php
+++ panel/modules/setting.php
@@ -1,6 +1,100 @@
 <div class="container-fluid">
-
-    <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800">Public Setting</h1>
-
+    <form method="post">
+        <div class="row">
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="firstnameId" aria-describedby="emailHelp" placeholder="Enter First Name...">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="lastnameId" aria-describedby="emailHelp" placeholder="Enter Last Name...">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="phoneId" aria-describedby="emailHelp" placeholder="Enter Phone Number...">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="emailId" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-9">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="addressId" aria-describedby="emailHelp" placeholder="Enter Postal Address...">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="websiteId" aria-describedby="emailHelp" placeholder="Enter Website Address...">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="instagramId" aria-describedby="emailHelp" placeholder="Enter Instagram Link...">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="linkdinId" aria-describedby="emailHelp" placeholder="Enter Linkdin Link...">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="facebookId" aria-describedby="emailHelp" placeholder="Enter Facebook Link...">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="twitterId" aria-describedby="emailHelp" placeholder="Enter Twitter Link...">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="githubId" aria-describedby="emailHelp" placeholder="Enter Github Link...">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="" aria-describedby="emailHelp" placeholder="">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="" aria-describedby="emailHelp" placeholder="">
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="" aria-describedby="emailHelp" placeholder="">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <textarea class="form-control form-control-user" name="about" placeholder="Enter about text..."></textarea>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="form-group">
+                    <textarea class="form-control form-control-user" name="intrest" placeholder="Enter intrest text..."></textarea>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <input type="submit" name="submitPublicSettingForm" class="btn btn-primary btn-user btn-block" value="Save Public Setting">
+            </div>
+        </div>
+    </form>
 </div>
