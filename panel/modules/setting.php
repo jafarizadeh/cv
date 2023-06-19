32980884ea267999e31c2ef1ba0af38e3803990b
Mehdi <mehdi0341@gmail.com>
2023-06-19 17:39:21 +0200

    setting -> mysqli_query

--- panel/modules/setting.php
+++ panel/modules/setting.php
@@ -1,3 +1,36 @@
+<?php
+$select = mysqli_query($GLOBALS['con'], "SELECT * FROM `general_info` WHERE `id` = '1' limit 1");
+$general_info_data = mysqli_fetch_array($select);
+
+function updatePublicSettingTable ()
+{
+    if (postParam('submitPublicSettingForm') != null)
+    {
+        $web_title = postParam('webTitle') != null ? postParam('webTitle') : '';
+        $first_name = postParam('firstName') != null ? postParam('firstName') : '';
+        $last_name = postParam('lastName') != null ? postParam('lastName') : '';
+        $telephone_number = postParam('tel') != null ? postParam('tel') : '';
+        
+        $email_address = postParam('email') != null ? postParam('email') : '';
+        $postal_address = postParam('address') != null ? postParam('adddress') : '';
+        $facebook_id = postParam('facebook') != null ? postParam('facebook') : '';
+        $linkedin_id = postParam('linkedin') != null ? postParam('linkedin') : '';
+        
+        $twitter_id = postParam('twitter') != null ? postParam('twitter') : '';
+        $github_id = postParam('github') != null ? postParam('github') : '';
+        $about = postParam('about') != null ? postParam('about') : '';
+        $interests = postParam('interests') != null ? postParam('interests') : '';
+
+        $select = mysqli_query($GLOBALS['con'], "UPDATE `general_info` SET `webTitle` = '$web_title',
+        `firstName` = '$first_name', `lastName` = '$last_name', `tel` = '$telephone_number', 
+        `email` = '$email_address', `address` = '$postal_address', `facebook` = '$facebook_id', 
+        `linkedin` = '$linkedin_id', `twitter` = '$twitter_id', `github` = '$github_id',
+         `about` = '$about', `interests` = '$interests'");
+        
+    }
+}
+?>
+
 <div class="container-fluid">
     <h1 class="h3 mb-4 text-gray-800">Public Setting</h1>
     <form method="post">
