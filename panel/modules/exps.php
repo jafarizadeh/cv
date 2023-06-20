37dd2be06de9c3d5d3f6e3aa4e3c9959de0dec37
Mehdi <mehdi0341@gmail.com>
2023-06-20 10:44:28 +0200

    show exp

---
+++ panel/modules/exps.php
@@ -0,0 +1,7 @@
+<?php
+$select = mysqli_query($GLOBALS['con'], "SELECT * FROM `experience` ORDER BY `id` DESC");
+?>
+
+<div class="container-fluid">
+    <h1 class="h3 mb-4 text-gray-800">Experiences</h1>
+</div>
