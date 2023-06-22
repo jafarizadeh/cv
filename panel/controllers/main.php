a2fe1c963fd1056347fa2e7d6491d21104415689
Mehdi <mehdi0341@gmail.com>
2023-06-22 21:35:34 +0200

    photo

--- panel/controllers/main.php
+++ panel/controllers/main.php
@@ -1,8 +1,4 @@
 <?php
-$PANEL_ROUTE_MAIN_ADR = 'index.php?module=';
-$HOST_ADR = 'http://localhost/project/cv/';
-$HOST_PANEL_ADR = $HOST_ADR.'panel/';
-$HOST_PANEL_UPLOAD_ADR = $HOST_PANEL_ADR.'uploads/';
 
 function getParam($param_name)
 {
